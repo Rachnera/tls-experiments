59b88973babef80075dd2d45774106315eec97eb
Rachnera <arachne.silk@gmail.com>
2024-04-06 18:11:17 +0200

    Can configure custom faces depending on attacks

--- Rachnera Battle.rb
+++ Rachnera Battle.rb
@@ -1,10 +1,27 @@
 # Need to be after the "Full Bleed" script and all the other many battle scripts
+module Busty
+  BATTLE_CONFIG = {
+    "Simon2" => {
+      "Commanding Presence" => {
+        face_name: "1 Simon dark eyes",
+        face_index: 0,
+      },
+    },
+    "Yarra" => {
+      "Succubus Kiss" => {
+        face_name: "Yarra emo2",
+        face_index: 6,
+      },
+    },
+  }
+end
 class Scene_Battle < Scene_Base
   alias original_478_show_animation show_animation
   def show_animation(targets, animation_id)
     bust = Busty::Bust.new(999)
     if show_bust?
-      bust.draw(character_name, -60, 60, @subject.face_name, @subject.face_index)
+      # TODO Replace hardcoded x/y with configurable ones
+      bust.draw(character_name, -64, 64, bust_face[:face_name], bust_face[:face_index])
     end
 
     original_478_show_animation(targets, animation_id)
@@ -18,6 +35,14 @@ class Scene_Battle < Scene_Base
     return false unless @subject.is_a?(Game_Actor)
 
     Busty::has_bust?(character_name)
+  end
+
+  def bust_face
+    if Busty::BATTLE_CONFIG[character_name] and Busty::BATTLE_CONFIG[character_name][@subject.current_action.item.name]
+      return Busty::BATTLE_CONFIG[character_name][@subject.current_action.item.name]
+    end
+
+    { face_name: @subject.face_name, face_index: @subject.face_index }
   end
 
   def character_name
